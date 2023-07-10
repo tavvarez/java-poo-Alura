4f266b6de74fc4d82b1185ea03e6a1baece4c5ac
tavvarez <gabrieldaraujotavares@gmail.com>
2023-07-10 01:44:40 -0300

    obtendo classes e objetos

---
+++ screenmatch/src/Filme.java
@@ -0,0 +1,26 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double somaDasAvaliacoes;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+
+    void exibeFichaTecnica () {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " +duracaoEmMinutos);
+        System.out.println("Incluído no plano: " + incluidoNoPlano);
+
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double obterMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
---
+++ screenmatch/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O poderoso chefão";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(7);
+        meuFilme.avalia(10);
+        System.out.println("Média de avaliações do filme: " + meuFilme.obterMedia());
+    }
+}
