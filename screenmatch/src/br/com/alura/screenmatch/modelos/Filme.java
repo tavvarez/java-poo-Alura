e90064e5eb1c6a4372d13231a5c8aad8da7b59cd
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-07-17 20:36:03 -0300

    criando herança | substituindo filme por titulo

--- screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,48 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
-public class Filme {
-    private String nome;
-    private int anoDeLancamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totalDeAvaliacoes;
-    private int duracaoEmMinutos;
+public class Filme extends Titulo {
+    private String diretor;
 
-    public int getTotalDeAvaliacoes() {
-        return totalDeAvaliacoes;
-    }
-    
-    // setters
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-  
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
-    }
-
-    public void exibeFichaTecnica () {
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        System.out.println("Duração em minutos: " +duracaoEmMinutos);
-        System.out.println("Incluído no plano: " + incluidoNoPlano);
-
-    }
-
-    public void avalia(double nota) {
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-    }
-
-    public double obterMedia() {
-        return somaDasAvaliacoes / totalDeAvaliacoes;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
