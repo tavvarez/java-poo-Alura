200b8ebef2a476af0a5e147b6b7ea07564cb87a4
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-07-17 20:11:49 -0300

    incluindo setters

--- screenmatch/src/Main.java
+++ screenmatch/src/Main.java
@@ -3,9 +3,9 @@ import br.com.alura.screenmatch.modelos.Filme;
 public class Main {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O poderoso chefão";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(8);
--- screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,14 +1,35 @@
 package br.com.alura.screenmatch.modelos;
+
 public class Filme {
-    String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
-    double somaDasAvaliacoes;
-    int totalDeAvaliacoes;
-    int duracaoEmMinutos;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
 
+    public int getTotalDeAvaliacoes() {
+        return totalDeAvaliacoes;
+    }
+    
+    // setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+  
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
 
-    void exibeFichaTecnica () {
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica () {
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de lançamento: " + anoDeLancamento);
         System.out.println("Duração em minutos: " +duracaoEmMinutos);
@@ -16,12 +37,12 @@ public class Filme {
 
     }
 
-    void avalia(double nota) {
+    public void avalia(double nota) {
         somaDasAvaliacoes += nota;
         totalDeAvaliacoes++;
     }
 
-    double obterMedia() {
+    public double obterMedia() {
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
 }
