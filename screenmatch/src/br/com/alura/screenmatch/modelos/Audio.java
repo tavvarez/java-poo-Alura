7dcb45e629b8b0bb47b1e0a62db8c5747a69e520
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-07-24 12:49:55 -0300

    criando classe para podcast

---
+++ screenmatch/src/br/com/alura/screenmatch/modelos/Audio.java
@@ -0,0 +1,21 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Audio extends Titulo {
+    private int audio;
+    private int totalReproducoes;
+    
+    public int getAudio() {
+        return audio;
+    }
+    public void setAudio(int audio) {
+        this.audio = audio;
+    }
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+    public void setTotalReproducoes(int totalReproducoes) {
+        this.totalReproducoes = totalReproducoes;
+    }
+
+    
+}
