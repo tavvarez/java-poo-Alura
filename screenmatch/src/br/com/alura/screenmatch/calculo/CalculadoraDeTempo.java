226a6e2c31c832bb817724c9f13fda63d0846f4c
Tavvarez <gabrieldaraujotavares@gmail.com>
2023-07-24 02:50:45 -0300

    calculadora de tempo para assistir o filme

---
+++ screenmatch/src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
@@ -0,0 +1,22 @@
+package br.com.alura.screenmatch.calculo;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+public class CalculadoraDeTempo {
+    private int tempoTotal;
+
+    public int getTempoTotal() {
+        return tempoTotal;
+    }
+
+    // public void inclui(Filme filme) {
+    //     tempoTotal += filme.getDuracaoEmMinutos();
+    // }
+
+    // public void inclui(Serie serie) {
+    //     tempoTotal += serie.getDuracaoEmMinutos();
+    // }
+
+    public void inclui(Titulo titulo) {
+        tempoTotal += titulo.getDuracaoEmMinutos();
+    }
+}
